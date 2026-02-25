479d160f969495d704c7f0aa8ebe973ce679d467
mjmuhamm <53790225+mjmuhamm@users.noreply.github.com>
2026-02-25 09:05:27 -0500

    submission

--- app/src/main/java/com/example/spaceapi/MainActivity.kt
+++ app/src/main/java/com/example/spaceapi/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,10 +12,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -38,18 +35,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.example.spaceapi.api.SecondPageApi
 import com.example.spaceapi.model.firstPage.Result
-import com.example.spaceapi.model.secondPage.SecondResponse
 import com.example.spaceapi.ui.theme.SpaceAPITheme
 import com.example.spaceapi.viewModel.SecondState
 import com.example.spaceapi.viewModel.SecondViewModel
@@ -60,7 +51,6 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel: SpaceViewModel by viewModels()
     private val viewModel2: SecondViewModel by viewModels()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -159,19 +149,6 @@ fun SecondPage(id: String, viewModel: SecondViewModel, onNavigate: () -> Unit = 
                                             .clickable(onClick = onNavigate)
 
                                     )
-
-                                    // Conditionally show the new composable
-
-//                    Text(
-//                        "<",
-//                        color = Color.Green,
-//                        fontSize = 50.sp,
-//                        fontFamily = fonts,
-//                        fontWeight = FontWeight.Light,
-//                        modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 0.dp).clickable {
-//
-//                        }
-//                    )
 
                                     Text(
                                         "Second Page",
